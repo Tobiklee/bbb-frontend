0d768ff327c137b19ce35be7ef93a4697321c3d5
Tobias Kleemann <tobiaskleemann1995@gmail.com>
2020-12-18 16:26:04 +0100

    side navigation

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,18 @@
       scroll-target="#scrolling-techniques-2"
     >
       <img class="mt-3 w-24 h-24" src="./assets/fedora-hut.png" />
-      <div class="flex flex-col space-y-2 h-full justify-center">
-        <v-btn><nav-router-link to="/">Anmeldung!</nav-router-link> </v-btn>
-        <v-btn>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" dark absolute temporary>
+      <div class="flex flex-col align-center">
+        <v-btn class="w-full">
+          <nav-router-link to="/">Anmeldung! </nav-router-link>
+        </v-btn>
+        <v-btn class="w-full">
           <nav-router-link to="/fights">Zu den Kämpfen!</nav-router-link>
         </v-btn>
       </div>
-    </v-app-bar>
+    </v-navigation-drawer>
     <v-main class="bg-gray-800">
       <v-container>
         <router-view />
@@ -30,5 +35,10 @@ import NavRouterLink from "./components/NavRouterLink.vue";
 
 export default {
   components: { NavRouterLink },
+  data() {
+    return {
+      drawer: false,
+    };
+  },
 };
 </script>
